3b2cc498457653ca150853425a8460d51ce147fe
VirginiaZhang <zhangmiger@gmail.com>
2019-01-17 22:06:01 +0100

    Update Setup Scripts by CC.NET

--- TestGit/TestGit/Properties/AssemblyInfo.cs
+++ TestGit/TestGit/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.f42125624d0ed6b60b4875dc329bbf37da1409c2")]
